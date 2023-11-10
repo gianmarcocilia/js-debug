80823983ac8fc10af07d4f62647f718661438b1a
gianmarcocilia <gianmarcocilia97@gmail.com>
2023-11-10 16:13:20 +0100

    add: esercizio 3 e 4

--- js-debug/bundle-01/main.js
+++ js-debug/bundle-01/main.js
@@ -30,21 +30,33 @@ function addIfEven(num) {
 
 // ESERCIZIO 3
 function loopToFive() {
-    for (let i = 0, i < 5, i++) {
+    for (let i = 1; i <= 5; i++) {
         console.log(i);
     }
 }
+
+/**L'errore in questo codice sono le , al posto dei ; e settando i = 1 e i <= 5 andremo a contare da 1 a 5*/
 
 
 // ESERCIZIO 4 (suggerimento: ci sono 7 errori)
 function displayEvenNumbers() {
     let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
     let evenNumbers = [];
-    for (let i = 0; i < numbers.length - 1; i++;) {
-        if (numbers % 2 = 0); {
-            evenNumbers.push(i);
+    for (let i = 0; i < numbers.length; i++) {
+        const elem = numbers[i];
+        if (elem % 2 === 0) {
+            evenNumbers.push(elem);
         }
-        return evenNumbers;
     }
+    return evenNumbers;
 }
 displayEvenNumbers(); // dovrebbe restituire [2,4,6,8]
+
+/**errore 1: il ; dopo i++ 
+ * errore 2: un singolo = che significa assegnamento
+ * errore 3: stiamo andando a dividere per 2 l'array e non gli elementi all'interno
+ * errore 4: non stiamo scorrendo l'array fino all'ultimo elemento
+ * errore 5: stiamo pushando nell'array l'indice e non il valore
+   errore 6: il return va fuori dal ciclo for
+   errore 7: ; dopo la condizione 
+*/
